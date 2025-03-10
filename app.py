444b7c41f80f82ebbd2278355951eb07bb5e7a8e
Anton <cantillanoanton0@gmail.com>
2025-03-10 21:54:48 +1300

    change home page message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     #home page
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, again!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
